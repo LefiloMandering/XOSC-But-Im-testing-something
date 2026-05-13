bb79010569c3de990615a362888d66697fc547a2
hollyn <hollynn@kittymail.com>
2026-05-13 12:00:30 -0500

    Release build

--- publish.cmd
+++ publish.cmd
@@ -20,7 +20,7 @@ if !errorlevel! equ 0 (
 REM Create and push a release tag
 if exist "%OUT%\XOSC.zip" (
     for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-    for /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+    for /f "tokens=1-2 delims=: " %%a in ('time /t') do (set mytime=%%a-%%b)
     
     set "TAG=release-!mydate!-!mytime!"
     
